d3cef6c670f37fd904e4858b1aafd671a2b61e77
kristina <k@wooster.earthlink.net>
2008-07-25 15:14:54 -0400

    fixed a couple nulls

--- app/main.js
+++ app/main.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -58,7 +58,8 @@ function main() {
 		else {
 			var template = JSDOC.opt.t || System.getProperty("jsdoc.template.dir");
 
-			var handler = jsdoc.symbolSet.handler;
+                        if(!jsdoc || !jsdoc.symbolSet) jsdoc = {symbolSet : {handler : null}};
+                        var handler = jsdoc.symbolSet.handler;
 			if (handler && handler.publish) {
 				handler.publish(jsdoc.symbolSet);
 			}
