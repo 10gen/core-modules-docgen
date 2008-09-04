0fb5553867160a4d3572c12a72e2d6b5bf12d3bb
kristina <k@wooster.earthlink.net>
2008-09-04 10:48:52 -0400

    djang10 template for doc

---
+++ apidocs/_init.js
@@ -0,0 +1,3 @@
+
+core.modules.djang10.install();
+djang10.addTemplateRoot( core.modules.docgen.apidocs );
---
+++ apidocs/methods.js
@@ -0,0 +1,93 @@
+
+Methods = {
+
+    getArrayLinks : function( array ) {
+        return array.sort().map( function($) { return Methods.linkToSymbol($); } ).join(", ");
+    },
+
+    linkToSymbol : function( symbol, pretty ) {
+        var name = Methods.getName( symbol );
+        log("name: "+name);
+        if( !pretty ) {
+            if( symbol.alias )
+                pretty = symbol.alias;
+            else
+                pretty = name; 
+        }
+        return '<a href="'+name+'">'+pretty+'</a>';
+    },
+
+    makeSignature : function( symbol ) {
+        var params;
+        if( symbol.params )
+            params = symbol.params;
+        else 
+            params = symbol._params;
+
+        return "("+params.map( function($) { return " "+$.type+" "+$.name+" "; } ).join(", ")+")";
+    },
+
+    makeSrcLink : function( code ) {
+        link = code.replace( /\//g, "." );
+        log("code: "+code+" link: "+link);
+        return '<a href="code='+link+'">'+code+'</a>';
+    },
+
+    getBorrowers : function( array ) {
+       return array.filter(function($) {return $.memberOf != data.alias});
+    },
+
+    getUnique : function( array, field ) {
+        u = [];
+        array.map(function($) {
+            if (u.indexOf ($[field] ) < 0) 
+                u.push( $[field] )
+        });
+        return u;
+    },
+
+    filterEqual : function( array, field, val ) {
+        array.filter(function($) { return $[field] == val });
+    },
+
+    filterNot : function( array, field ) {
+        return array.filter(function($){return !$.isNamespace; } );
+    },
+
+    anonClassLink : function( alias, symbol ) {
+        var name = Methods.getName( symbol );
+        return '<a href="'+alias+"#"+name+'">'+name+'</a>';
+    },
+
+    relativeLink : function( symbol, text, num ) {
+        var name = Methods.getName( symbol );
+        var alias = symbol.alias ? symbol.alias : name;
+        var anchor = text ? ( num ? text+num : text ) : name;
+        return '<a href="'+alias+"#"+anchor+'">'+name+'</a>';
+    },
+
+    linkAnchor : function( symbol, text, num ) {
+        var name = Methods.getName( symbol );
+        var anchor = text ? ( num ? text+num : text ) : name;
+        return '<a name="'+anchor+'"/>';
+    },
+
+    resolveLinks : function( text ) {
+        return text;
+    },
+
+    summarize : function( text ) {
+        return text.substring(0, text.indexOf(". "));
+    },
+
+    getName : function( symbol ) {
+        var name;
+        if( symbol.name ) 
+            name = symbol.name;
+        else if( symbol._name )
+            name = symbol._name;
+        else 
+            name = symbol;
+        return name;
+    }
+}
