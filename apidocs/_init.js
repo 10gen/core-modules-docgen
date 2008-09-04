4b1038154980985e456f69ff853a0970e0c180c5
kristina <k@wooster.earthlink.net>
2008-09-04 11:24:58 -0400

    Only generate the sidebar once

--- apidocs/_init.js
+++ apidocs/_init.js
@@ -1,3 +1,5 @@
 
 core.modules.djang10.install();
 djang10.addTemplateRoot( core.modules.docgen.apidocs );
+
+core.modules.docgen.apidocs.sidebar();
---
+++ apidocs/sidebar.js
@@ -0,0 +1,49 @@
+
+Sidebar = {};
+Sidebar.pkgs = {};
+
+// traverse the tree to print the classes
+Sidebar.printSidebar = function(curPkg, curPath, indent) {
+    for(var k in curPkg) {
+        print('<div>');
+        print('<a href="' + (curPath+k) +'" style="font-weight: normal; font-size: 100%; margin-left: '+ indent +'em;">' + k + '</a>');
+        print('</div>');
+        Sidebar.printSidebar(curPkg[k], curPath+k+".", indent+1);
+    }
+}
+
+
+cursor = db.doc.find().sort({name:1});
+
+var checkIfExists = function(h, idx) {
+    while(h.length > idx+1) h.pop();
+    var path = h.join(".");
+    var obj = db.doc.findOne({name : path});
+    // if there is not already an object with this name, create a placeholder
+    if (obj == null) {
+        obj = {
+            _index : {},
+            content : '<br /><div id="content"><h1 class="classTitle">'+path+'</h1><p class="description">There is no documentation for this package, yet.</p></div>',
+            name : path,
+            desc : "",
+            ts: new Date(),
+            version : null
+        };
+        db.doc.save(obj);
+    }
+}
+
+// create a table based on the class heirarchy
+cursor.forEach( function(blob) {
+    var curPkg = Sidebar.pkgs;
+    var heirarchy = blob.name.split(".");
+    for(var i=0; i<heirarchy.length; i++) {
+        if( !(heirarchy[i] in curPkg) ) {
+            checkIfExists(Object.extend([], heirarchy), i);
+            curPkg[heirarchy[i]] = {};
+        }
+        curPkg = curPkg[heirarchy[i]];
+    }
+});
+
+
